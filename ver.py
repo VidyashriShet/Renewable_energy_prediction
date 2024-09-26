4d62d81ffe122b1eb50e373f2c0de5bd79df937a
vidyashri <vidyashri7799@gmail.com>
2024-09-26 22:04:32 +0530

    initial commit

---
+++ ver.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import tensorflow as tf
+
+
+print(np.__version__)
+print(pd.__version__)
+print(sklearn.__version__)
+print(tf.__version__)
